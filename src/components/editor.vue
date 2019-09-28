ff857a6a59451268bcc9f1a65443e68164b89899
xuchaichai <767733103@qq.com>
2019-09-28 17:04:30 +0800

    2019.9.28

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -3,15 +3,46 @@
     <div class="editor_title">
       <Input v-model="title" placeholder="请输入文章标题" style="width: 47%" />
       <Input v-model="title1" placeholder="请输入文章副标题" style="width: 47%" />
-      <Button class="editor_btn" type="primary" style="width: 5%" @click="pull" >发布</Button> 
+      <Button class="editor_btn" type="primary" style="width: 5%" @click="pull">发布</Button>
     </div>
-    <div id="editor" class="editor1" ></div>
+    <div class="editor_main">
+      <v-scroll>
+        <div id="editor" class="editor1"></div>
+      </v-scroll>
+    </div>
+
+    <div class="editor_images_group">
+      <v-scroll>
+        <div class="editor_images">
+          <div class="editor_image editor_image_bg" v-for="(item,index) in blobArr" :key="index">
+            <img :src="item.url" alt />
+            <Icon class="editor_del" type="md-close" @click="delEditorBlob(index)"/>
+            <viewer class="editor_img_view">
+              <img :src="item.url" />
+            </viewer>
+          </div>
+          <div class="editor_image" v-if="blobArr.length < 10">
+            <img src="../../static/images/add.png" alt />
+            <input multiple type="file" accept="image/*" @change="pushImage" />
+          </div>
+        </div>
+      </v-scroll>
+    </div>
   </div>
 </template>        
 
 <script>
 // require styles
 import E from "wangeditor";
+import { async } from 'q';
+
+import 'viewerjs/dist/viewer.css'
+  import Viewer from 'v-viewer'
+  import Vue from 'vue'
+  Vue.use(Viewer)
+  Viewer.setDefaults({
+    Options: { 'inline': false, 'button': false, 'navbar': false, 'title': false, 'toolbar': false, 'tooltip': false, 'movable': false, 'zoomable': false, 'rotatable': false, 'scalable': false, 'transition': false, 'fullscreen': false, 'keyboard': false, 'url': 'data-source' }
+  })  
 
 export default {
   name: "editor",
@@ -20,27 +51,33 @@ export default {
   },
   data() {
     return {
-      title: '',
-      title1: '',
-      formArticle: ''
+      title: "",
+      title1: "",
+      formArticle: "",
+      maxLength: 10,
+      blobArr: [
+        // {
+        //   name: 'xxx.png',
+        //   type: 'png',
+        //   blob: 'http://xxxxx'
+        // },
+      ],
+      fileArr: [],
+      upIndex: 0,
+      upNum: 0,
     };
   },
   methods: {
-    onEditorBlur() {
-      //失去焦点事件
-    },
-    onEditorFocus() {
-      //获得焦点事件
-    },
-    onEditorReady() {},
-    onEditorChange() {
-      //内容改变事件
+    delEditorBlob(index) {
+      console.log(index)
+      let copyBlobArr = this.blobArr;
+      copyBlobArr.splice(index,1);
+      this.blobArr = copyBlobArr;
     },
     pull() {
-      if(this.title == '' || this.title1 == '' || this.formArticle == '') {
-        this.$Message.warning('内容请填写完整')
-      }
-      else {
+      if (this.title == "" || this.title1 == "" || this.formArticle == "") {
+        this.$Message.warning("内容请填写完整");
+      } else {
         this.pullArticle();
       }
     },
@@ -77,11 +114,11 @@ export default {
             article_title: this.title,
             article_sub_title: this.title1,
             article_post_time: targeDate,
-            article_author: 
-              JSON.parse(localStorage.getItem("info")).username,
+            article_author: JSON.parse(localStorage.getItem("info")).username,
             article_content: this.formArticle,
             article_background: "",
-            article_avatar: JSON.parse(localStorage.getItem("info")).ext_info.yesapi_avatar,
+            article_avatar: JSON.parse(localStorage.getItem("info")).ext_info
+              .yesapi_avatar,
             update_time: targeDate,
             article_zan: JSON.stringify([]),
             article_star: JSON.stringify([]),
@@ -93,18 +130,68 @@ export default {
             showArticle: true
           }),
           add_time: targeDate,
-          update_time: targeDate,
+          update_time: targeDate
         })
         .then(res => {
           console.log(res);
-          if(res.data.err_code == 0) {
-            this.$Message.success('发布成功')
+          if (res.data.err_code == 0) {
+            this.$Message.success("发布成功");
           }
         })
         .catch(res => {
           console.log(res);
         });
     },
+
+    // 把图片push到数组中，方便提交时处理
+    pushImage(e) {
+      console.log(e);
+      let that = this;
+      let upArr = e.target.files;
+      this.upIndex = 0;
+      this.upNum = upArr.length;
+      if(this.upNum.length < 1) {
+        return false;
+      }
+      async function ups() {
+        if(that.blobArr.length >= 10) {
+          that.$Message.warning('最多可以上传10张图片')
+        }
+        else {
+          if(that.upIndex > that.upNum-1) {
+            that.$Message.success('完成');
+          }
+          else {
+            await pushAsync(upArr);
+          }
+        }
+      }
+
+      ups();
+
+      function pushAsync(arr) {
+        let blob = new Blob([arr[that.upIndex]]);
+        let url = URL.createObjectURL(blob);
+        // console.log(arr[that.upIndex].name)
+        // let names = arr[that.upIndex].name
+        let obj = {
+          url: url,
+          info: arr[that.upIndex]
+          // imgName: names,
+          // imgType: arr[that.upIndex].type
+        }
+        that.blobArr.push(obj);
+        that.fileArr.push(arr[that.upIndex]);
+        that.upIndex++;
+        ups();
+      }
+
+      // let formData = new FormData();
+      // formData.append('image',val);
+      // let blob = new Blob([val]);
+      // let url = URL.createObjectURL(blob);
+    },
+
   },
   beforeCreate() {},
   created() {},
@@ -112,37 +199,39 @@ export default {
   mounted() {
     var editor = new E("#editor");
     editor.customConfig.colors = [
-        '#000000',
-        '#eeece0',
-        '#1c487f',
-        '#4d80bf',
-        '#c24f4a',
-        '#8baa4a',
-        '#7b5ba1',
-        '#46acc8',
-        '#f9963b',
-        '#ffffff',
-        '#ffsffs'
-    ]
+      "#000000",
+      "#eeece0",
+      "#1c487f",
+      "#4d80bf",
+      "#c24f4a",
+      "#8baa4a",
+      "#7b5ba1",
+      "#46acc8",
+      "#f9963b",
+      "#ffsffs",
+      "#ffffff"
+    ];
+    editor.customConfig.menus = [
+      // 'head',  // 标题
+      "bold", // 粗体
+      "fontSize", // 字号
+      "fontName", // 字体
+      "italic", // 斜体
+      "underline", // 下划线
+      "strikeThrough", // 删除线
+      "foreColor", // 文字颜色
+      "backColor", // 背景颜色
+      "list", // 列表
+      "justify", // 对齐方式
+      "quote", // 引用
+      "emoticon", // 表情
+      "table", // 表格
+      "code", // 插入代码
+      "undo", // 撤销
+      "redo" // 重复
+    ];
     editor.customConfig.onchange = html => {
       this.formArticle = html;
-    };
-    editor.customConfig.uploadImgServer = "<%=path%>/Img/upload"; //上传URL
-    editor.customConfig.uploadImgMaxSize = 3 * 1024 * 1024;
-    editor.customConfig.uploadImgMaxLength = 5;
-    editor.customConfig.uploadFileName = "myFileName";
-    editor.customConfig.uploadImgHooks = {
-      customInsert: function(insertImg, result, editor) {
-        // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
-        // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
-
-        // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
-        var url = result.data;
-        console.log(url)
-        // insertImg(url);
-
-        // result 必须是一个 JSON 格式字符串！！！否则报错
-      }
     };
     editor.create();
   },
@@ -188,21 +277,118 @@ export default {
   /* background-color: white; */
 }
 
+.editor_main {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  right: 0;
+  top: 40px;
+  bottom: 125px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 8px;
+  overflow: hidden;
+}
+
 .editor1 {
   width: 100%;
-  height: 100%;
-  position: relative;
+  min-height: 100%;
+  height: auto;
+  position: absolute;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
   /* border: 1px solid white; */
   /* margin-top: 10px; */
   /* box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3); */
 }
 
+.editor_images_group {
+  position: absolute;
+  bottom: 0;
+  height: 120px;
+  left: 0;
+  right: 0;
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.editor_images {
+  position: absolute;
+  bottom: 0;
+  height: 120px;
+  left: 0;
+  /* right: 0; */
+  width: auto;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  /* background-color: rgba(255, 255, 255, 0.2); */
+  /* justify-content: space-between; */
+}
+
+.editor_image {
+  height: 100px;
+  width: 100px;
+  position: relative;
+  box-sizing: border-box;
+  border: 3px solid rgba(255, 255, 255, 1);
+  margin: 5px 6px;
+  overflow: hidden;
+  border-radius: 6px;
+  top: -2px;
+  /* cursor: pointer; */
+}
+
+.editor_image img {
+  width: 50%;
+  height: 50%;
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  z-index: 9;
+}
+
+.editor_image input {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 20;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.editor_image_bg img {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.editor_image .editor_del {
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 10;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  z-index: 20;
+}
+
+.editor_image .editor_img_view {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+}
+
 .w-e-toolbar .w-e-menu i {
-  color: white!important;
+  color: white !important;
 }
 
 .w-e-toolbar {
-  background-color: white!important;
+  background-color: white !important;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -28,8 +28,46 @@ Vue.use(vuescroll, {
       detectResize: true
     },
     scrollPanel: {
-      scrollingX: false,
+      scrollingX: true,
       scrollingY: true,
+      speed: 300,
+      easing: 'easeInQuint'
+    },
+    rail: {
+      background: 'rgba(255,255,255,0)',
+      opacity: 0,
+      size: '6px',
+      specifyBorderRadius: false,
+      gutterOfEnds: null,
+      gutterOfSide: '2px',
+      keepShow: false
+    },
+    bar: {
+      showDelay: 1000,
+      onlyShowBarOnScroll: false,
+      keepShow: true,
+      background: 'rgb(255,255,255)',
+      opacity: 1,
+      hoverStyle: false,
+      specifyBorderRadius: false,
+      minSize: false,
+      size: '5px',
+      disable: false,
+    }
+  }, // 在这里设置全局默认配置
+  name: 'v-scroll' // 在这里自定义组件名字，默认是vueScroll
+}); 
+
+Vue.use(vuescroll, {
+  ops: {
+    vuescroll: {
+      mode: 'native',
+      sizeStrategy: 'percent',
+      detectResize: true
+    },
+    scrollPanel: {
+      scrollingX: true,
+      scrollingY: false,
       speed: 300,
       easing: 'easeInQuint'
     },
@@ -55,8 +93,8 @@ Vue.use(vuescroll, {
       disable: false,
     }
   }, // 在这里设置全局默认配置
-  name: 'v-scroll' // 在这里自定义组件名字，默认是vueScroll
-});  
+  name: 'v-scrollx' // 在这里自定义组件名字，默认是vueScroll
+}); 
 
 
 
